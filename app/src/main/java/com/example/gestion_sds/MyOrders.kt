864add7346fda1db4e0fb2f7285a2f3bb5952780
Fida Sassi <fidaasassi@gmail.com>
2024-11-12 21:18:18 +0100

    update corrections for code

--- app/src/main/java/com/example/gestion_sds/MyOrders.kt
+++ app/src/main/java/com/example/gestion_sds/MyOrders.kt
@@ -11,10 +11,6 @@ class MyOrders : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_my_orders)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
