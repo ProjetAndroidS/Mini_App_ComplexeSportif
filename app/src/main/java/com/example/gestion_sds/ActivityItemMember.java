fbbad513c906001423617e5645a2777146bfdd35
Fida Sassi <fidaasassi@gmail.com>
2024-11-15 23:05:12 +0100

    Frontend done

---
+++ app/src/main/java/com/example/gestion_sds/ActivityItemMember.java
@@ -0,0 +1,26 @@
+package com.example.gestion_sds;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.servicelocal.R;
+
+public class ActivityItemMember extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_item_member2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
