81fceeb8801afa3ff6d48e49c9c43a8042c58d3e
Fida Sassi <fidaasassi@gmail.com>
2024-11-15 23:42:44 +0100

    set up buttons : login, sign up , main

--- app/src/main/java/com/example/gestion_sds/ActivityLogin.java
+++ app/src/main/java/com/example/gestion_sds/ActivityLogin.java
@@ -1,12 +1,10 @@
 package com.example.gestion_sds;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.servicelocal.R;
 
@@ -15,12 +13,16 @@ public class ActivityLogin extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Initialize the Login button
+        Button loginButton = findViewById(R.id.SignUp); // Assurez-vous que l'ID est correct dans le fichier XML
+
+        // Set OnClickListener for the Login button
+        loginButton.setOnClickListener(v -> {
+            // Navigate to the Inscri activity
+            Intent intent = new Intent(ActivityLogin.this, ActivityInscri.class);
+            startActivity(intent);
         });
     }
 }
--- app/src/main/java/com/example/gestion_sds/ActivityMain.java
+++ app/src/main/java/com/example/gestion_sds/ActivityMain.java
@@ -1,12 +1,12 @@
 package com.example.gestion_sds;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.servicelocal.R;
 
@@ -17,6 +17,16 @@ public class ActivityMain extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        Button btnLogin = findViewById(R.id.btnLogin);
 
+        // Ajout de l'OnClickListener pour naviguer vers login.xml
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Création de l'Intent pour passer à l'Activity de login
+                Intent intent = new Intent(ActivityMain.this, ActivityLogin.class);
+                startActivity(intent);
+            }
+        });
     }
 }
